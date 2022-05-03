adec72500f7185a801b5078f193a2953cc0d98b6
angelinahan <han.ang@northeastern.edu>
2022-05-03 12:23:05 -0400

    Alt text, clean up my zine for Aileen (test)

--- src/App.js
+++ src/App.js
@@ -55,37 +55,55 @@ const App = () => {
           <ItemContainer>
             <LowkeyLink to="/reilly">
               <TitleH2>01. Reilly</TitleH2>
-              <CoverImg src={img1} alt="covers" />
+              <CoverImg
+                src={img1}
+                alt="A scan of two zine covers each showing a flatlay outfit. On the left there is a gray long-sleeve shirt and light wash ripped jeans. On the right there is a black long sleeve shirt under a cream T-shirt with blue, red, and green designs on it, and black folded skinny jeans."
+              />
             </LowkeyLink>
           </ItemContainer>
           <ItemContainer>
             <LowkeyLink to="/holly">
               <TitleH2>02. Holly</TitleH2>
-              <CoverImg src={img2} alt="covers" />
+              <CoverImg
+                src={img2}
+                alt="A scan of two zine covers each showing a flatlay outfit. On the left there is a "
+              />
             </LowkeyLink>
           </ItemContainer>
           <ItemContainer>
             <LowkeyLink to="/aileen">
               <TitleH2>03. Aileen</TitleH2>
-              <CoverImg src={img3} alt="covers" />
+              <CoverImg
+                src={img3}
+                alt="A scan of two zine covers each showing a flatlay outfit."
+              />
             </LowkeyLink>
           </ItemContainer>
           <ItemContainer>
             <LowkeyLink to="/kyle">
               <TitleH2>04. Kyle</TitleH2>
-              <CoverImg src={img4} alt="covers" />
+              <CoverImg
+                src={img4}
+                alt="A scan of two zine covers each showing a flatlay outfit. The left shows a blue baseball shirt that says Billy Joel, and a denim jacket. The right shows a dark longsleeve under a cream T-shirt and orange-brown pants."
+              />
             </LowkeyLink>
           </ItemContainer>
           <ItemContainer>
             <LowkeyLink to="/ava">
               <TitleH2>05. Ava</TitleH2>
-              <CoverImg src={img5} alt="covers" />
+              <CoverImg
+                src={img5}
+                alt="A scan of two zine covers each showing a flatlay outfit. The left shows a red dress and ribbon, and the right shows a black turtleneck layered under a blue cardigan, with folded black pants and a pink scarf. "
+              />
             </LowkeyLink>
           </ItemContainer>
           <ItemContainer>
             <LowkeyLink to="/angelina">
               <TitleH2>Epilogue: Angelina</TitleH2>
-              <CoverImg src={img6} alt="covers" />
+              <CoverImg
+                src={img6}
+                alt="a flatlay with pair of black jeans on the left and a mid-length, A-line dark green coat on the right."
+              />
             </LowkeyLink>
           </ItemContainer>
         </ContentRow>
